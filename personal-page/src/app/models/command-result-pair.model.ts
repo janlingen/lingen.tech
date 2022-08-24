462789d35e0cd6bce323b5ce004af1b3873b6f0c
janlingen <82512348+janlingen@users.noreply.github.com>
2022-08-24 22:04:48 +0200

    Add functionality to CommandResultPair

--- personal-page/src/app/models/command-result-pair.model.ts
+++ personal-page/src/app/models/command-result-pair.model.ts
@@ -1,2 +1,17 @@
-export interface CommandResultPair {
+export class CommandResultPair {
+    private _command: string;
+    private _result: string;
+
+    constructor(command: string, result: string){
+        this._command = command;
+        this._result = result;
+    }
+
+    get command(): string{
+        return this._command;
+    }
+
+    get result(): string{
+        return this._result;
+    }
 }
