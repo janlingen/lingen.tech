c709ec7db78f57fcb96084595c4a84a9c796042a
janlingen <82512348+janlingen@users.noreply.github.com>
2022-08-24 22:12:05 +0200

    Add functionality for HistoryService

--- personal-page/src/app/services/history.service.ts
+++ personal-page/src/app/services/history.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
+import { CommandResultPair } from '../models/command-result-pair.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryService {
 
+  history: CommandResultPair[] = [];
   
+  constructor() {}
 
-  constructor() { }
+  showHistory(): string[]{
+    let commands: string[] = []
+    for(var pair of this.history){
+      commands.push(pair.result)
+    }
+    return commands;
+  }
 }
