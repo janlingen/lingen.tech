7b80d9466bf0700e2e69195aac13d2d55509e8f2
janlingen <82512348+janlingen@users.noreply.github.com>
2022-08-30 18:10:11 +0200

    Add light dark functionality to gui

--- personal-page/src/app/gui/graphical-interface/graphical-interface.component.ts
+++ personal-page/src/app/gui/graphical-interface/graphical-interface.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-graphical-interface',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./graphical-interface.component.css']
 })
 export class GraphicalInterfaceComponent implements OnInit {
+  status: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickEvent(){
+    this.status = !this.status;       
   }
 
 }
