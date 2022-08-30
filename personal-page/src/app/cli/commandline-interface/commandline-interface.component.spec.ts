0a40c66f4b892d92d44fa8a19e662e8e938e6ced
janlingen <82512348+janlingen@users.noreply.github.com>
2022-08-30 17:26:35 +0200

    Create CommandlineInterfaceComponent

---
+++ personal-page/src/app/cli/commandline-interface/commandline-interface.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CommandlineInterfaceComponent } from './commandline-interface.component';
+
+describe('CommandlineInterfaceComponent', () => {
+  let component: CommandlineInterfaceComponent;
+  let fixture: ComponentFixture<CommandlineInterfaceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CommandlineInterfaceComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CommandlineInterfaceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ personal-page/src/app/cli/commandline-interface/commandline-interface.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commandline-interface',
+  templateUrl: './commandline-interface.component.html',
+  styleUrls: ['./commandline-interface.component.css']
+})
+export class CommandlineInterfaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
