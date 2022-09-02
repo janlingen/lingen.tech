1c1e717feac2ed7055461c285142e903a492d40a
janlingen <82512348+janlingen@users.noreply.github.com>
2022-09-02 19:02:24 +0200

    Update HistoryService

--- personal-page/src/app/cli/services/history.service.ts
+++ personal-page/src/app/cli/services/history.service.ts
@@ -1,20 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CommandResultPair } from '../../models/command-result-pair.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HistoryService {
+  commandHistory: string[] = [];
+  resultHistory: string[] = [];
 
-  history: CommandResultPair[] = [];
-  
   constructor() {}
 
-  showHistory(): string[]{
-    let commands: string[] = []
-    for(var pair of this.history){
-      commands.push(pair.result)
-    }
-    return commands;
+  getCommandHistory(): string[]{
+    return this.commandHistory.slice();
+  }
+
+  getResultHistory(): string[]{
+    return this.resultHistory.slice();
   }
 }
