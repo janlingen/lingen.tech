cf02411ce3363fda488825e2df096900e75f462e
janlingen <82512348+janlingen@users.noreply.github.com>
2022-09-17 15:40:05 +0200

    Update Navigator to work based on the device of a user

--- personal-page/src/app/navigation/navigation/navigation.component.ts
+++ personal-page/src/app/navigation/navigation/navigation.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      this.router.navigate(['/gui']);
+    } else {
+      this.router.navigate(['/cli']);
+    }
   }
-
 }
