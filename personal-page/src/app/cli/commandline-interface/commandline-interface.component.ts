3aa6905633ab2fc19e16c89aae6e42845b9395bf
janlingen <82512348+janlingen@users.noreply.github.com>
2022-09-17 15:28:33 +0200

    Add more commands to commandline-interface

--- personal-page/src/app/cli/commandline-interface/commandline-interface.component.ts
+++ personal-page/src/app/cli/commandline-interface/commandline-interface.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { HistoryService } from '../services/history.service';
 
 @Component({
@@ -10,77 +11,106 @@ import { HistoryService } from '../services/history.service';
 export class CommandlineInterfaceComponent implements OnInit {
   currentCommand: string = '';
 
-  constructor(private historyService: HistoryService) {}
+  constructor(private historyService: HistoryService, private router: Router) {
+    this.checkCommand('banner');
+    this.checkCommand('help');
+  }
 
   ngOnInit(): void {}
 
   onKeypress(event: KeyboardEvent) {
     if (event.key === 'Enter') {
-      this.checkCommand();
+      this.completeCommand();
+      this.checkCommand(this.currentCommand);
       this.currentCommand = '';
     }
   }
 
-  checkCommand() {
-    switch (this.currentCommand.toLowerCase()) {
+  completeCommand() {
+    const commands = [
+      'help',
+      'clear',
+      'about',
+      'email',
+      'linkedin',
+      'github',
+      'projects',
+      'history',
+      'banner',
+      'gui'
+    ];
+    if (this.currentCommand.length >= 1) {
+      const matches = commands.filter((s) => s.startsWith(this.currentCommand));
+      if (matches.length >= 1) {
+        this.currentCommand = matches[0];
+      }
+    }
+  }
+
+  checkCommand(command: string) {
+    switch (command.toLowerCase()) {
       case 'help':
-        this.historyService.addCommandResult(this.currentCommand, [
-          'help --> show all viable commands',
-          'clear --> clears the commandline',
-          'about --> get a short description of myself',
-          'email --> to find out my mail and instantly send me one',
-          'linkedin --> to look up some of my work experience',
-          'github --> to look up my github profile',
-          'projects --> to see all the repositories of my personal projects beside work',
-          'history --> showing all commands you typed to far in this session',
+        this.historyService.addCommandResult(command, [
+          'help, clear, about, github, projects, email, linkedin, history, gui',
+          '---------------------------------------------------------------------------------------',
+          'You can press "Enter" after entering a character to trigger auto-completion.',
         ]);
         break;
       case 'clear':
         this.historyService.clearHistory();
         break;
       case 'about':
-        this.historyService.addCommandResult(this.currentCommand, [
-          `I am currently completing two Bachelor's degrees, one in Computer Science
-          and one in Business Administration. My particular interests are data science,
-          financial markets, business intelligence and blockchain technologies.`,
+        this.historyService.addCommandResult(command, [
+          `I am currently pursuing two bachelor's degrees, one in computer science
+          and one in business administration. My special interests are all kinds of programming tasks,
+          financial markets, business intelligence, chess and blockchain technologies.`,
         ]);
         break;
       case 'email':
         window.open('mailto:info@lingen.tech');
-        this.historyService.addCommandResult(this.currentCommand, [
+        this.historyService.addCommandResult(command, [
           'mailto:info@lingen.tech',
         ]);
         break;
       case 'linkedin':
-        this.historyService.addCommandResult(this.currentCommand, ['']);
+        window.open(
+          'https://www.linkedin.com/in/jan-hendrik-lingen-11a922236/'
+        );
+        this.historyService.addCommandResult(command, [
+          'Opening a new tab with linkedin-profile...',
+        ]);
         break;
       case 'github':
         window.open('https://github.com/janlingen');
-        this.historyService.addCommandResult(this.currentCommand, [
+        this.historyService.addCommandResult(command, [
           'Opening a new tab with github-profile...',
         ]);
         break;
       case 'projects':
         window.open('https://github.com/janlingen?tab=repositories');
-        this.historyService.addCommandResult(this.currentCommand, [
+        this.historyService.addCommandResult(command, [
           'Opening a new tab with github-repositories...',
         ]);
         break;
       case 'history':
         this.historyService.addCommandResult(
-          this.currentCommand,
+          command,
           this.historyService.getHistory()
         );
         break;
       case 'banner':
-        this.historyService.addCommandResult(this.currentCommand, ['']);
+        this.historyService.addCommandResult(command, ['banner']);
+        break;
+      case 'gui':
+        this.historyService.addCommandResult(command, ['navigating to gui']);
+        this.router.navigate(['/gui']);
         break;
       case '':
-        this.historyService.addCommandResult(this.currentCommand, ['']);
+        this.historyService.addCommandResult(command, ['']);
         break;
       default:
-        this.historyService.addCommandResult(this.currentCommand, [
-          `cli: command not found: ${this.currentCommand}`,
+        this.historyService.addCommandResult(command, [
+          `cli: command not found: ${command}`,
           `Type 'help' to see all viable commands.`,
         ]);
         break;
