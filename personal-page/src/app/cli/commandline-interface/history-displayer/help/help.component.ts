14292ceda4bf2ca811ada4d120ce41bd279e0102
janlingen <82512348+janlingen@users.noreply.github.com>
2022-10-24 20:56:56 +0200

    Update HelpComponent's way to generate the view

--- personal-page/src/app/cli/commandline-interface/history-displayer/help/help.component.ts
+++ personal-page/src/app/cli/commandline-interface/history-displayer/help/help.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
-  styleUrls: ['./help.component.css']
+  styleUrls: ['./help.component.css'],
 })
 export class HelpComponent implements OnInit {
+  commands: string[] = [
+    'help',
+    'clear',
+    'about',
+    'skills',
+    'github',
+    'projects',
+    'email',
+    'linkedin',
+    'history',
+    'gui',
+    'technologies',
+    'ls',
+  ];
 
-  constructor() { }
+  declarations: string[] = [
+    'show all viable commands',
+    'clear the commandline',
+    'get a short description of me',
+    'show some of my skills (click on the icons)',
+    'loop up my github-profile',
+    'take a look on all my public projects',
+    'send me a mail',
+    'look up my linkedin-profile',
+    'show all commands u have used to far',
+    'graphical user interface',
+    'more detailed than skills',
+    'short list of all commands',
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
