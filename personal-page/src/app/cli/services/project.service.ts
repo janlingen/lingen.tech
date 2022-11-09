c740d0a0a2dbea32ac5581346226e238d770ef34
janlingen <82512348+janlingen@users.noreply.github.com>
2022-11-09 18:37:23 +0100

    Add project filter functionality to ProjectService

--- personal-page/src/app/cli/services/project.service.ts
+++ personal-page/src/app/cli/services/project.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Project } from './project.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectService {
+  projects: Project[] = [
+    new Project(
+      'attendance-manager',
+      'a spring boot application with a web user interface (thymeleaf) to manage project participant absences in a university environment where students need to build daily attendance into their flexible schedule, which may be filled with exams or other appointments away from home',
+      'https://github.com/janlingen/attendance-manager',
+      ['java', 'spring', 'git', 'docker', 'sql']
+    ),
+  ];
+
   constructor() {}
+
+  getRelatedProjects(skill: string): Project[] {
+    return this.projects.filter((s) =>
+      s.relatedSkills.filter((s) => s === skill)
+    );
+  }
 }
