cb12de8a14460b5430ab7d8511064b6ade650ae6
janlingen <82512348+janlingen@users.noreply.github.com>
2022-11-13 19:34:00 +0100

    Add several projects to ProjectService

--- personal-page/src/app/cli/services/project.service.ts
+++ personal-page/src/app/cli/services/project.service.ts
@@ -17,14 +17,39 @@ export class ProjectService {
       'archunit-presets',
       'a few examples of recurring architectures in the daily work of a software developer',
       'https://github.com/janlingen/archunit-presets',
-      ['java', 'spring']
+      ['java', 'spring', 'git']
     ),
     new Project(
       'lingen.tech',
       'personal page, with a terminal like look and my favourite colors from the dracula theme',
       'https://github.com/janlingen/lingen.tech',
-      ['angular', 'typescript', 'git'],
-    )
+      ['angular', 'typescript', 'git']
+    ),
+    new Project(
+      'dungeon-game',
+      'command line based dungeon game (university project), run around and try not to get caught by monsters until you reach the exit',
+      'https://github.com/janlingen/dungeon_game',
+      ['c', 'git']
+    ),
+    new Project(
+      'commandline-stocks',
+      'small Bloomberg replica to retrieve relevant real-time stock data through yahoo-api and compare multiple stocks or indexes via the command line',
+      'https://github.com/janlingen/command_line_stocks',
+      ['python', 'git', 'requests', 'rich', 'tabulate', 'python-dotenv']
+    ),
+    new Project(
+      'toggle-hue',
+      'simple python script to toggle your Philips hue lights with speech recognition',
+      'https://github.com/janlingen/toggle-hue',
+      [
+        'python',
+        'git',
+        'requests',
+        'SpeechRecognition',
+        'pyaudio',
+        'python-dotenv',
+      ]
+    ),
   ];
 
   constructor() {}
