3f0658fb39dae8ff008472be391ebbb753af46c4
janlingen <82512348+janlingen@users.noreply.github.com>
2022-11-21 21:37:51 +0100

    Update functionality in LibService

--- personal-page/src/app/cli/services/libs.service.ts
+++ personal-page/src/app/cli/services/libs.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Lib } from './lib.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LibsService {
 
+  libs: Lib[] = [new Lib('spring data', ['java', 'spring'])]
+
   constructor() { }
+
+  getRelatedLibs(skill: string): Lib[] {
+    return this.libs.filter((s) => s.relatedSkills.includes(skill));
+  }
 }
+  
