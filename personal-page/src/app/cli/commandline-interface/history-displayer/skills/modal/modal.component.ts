a3b044c60a4afec9cb367186e123b14bff990033
janlingen <82512348+janlingen@users.noreply.github.com>
2022-11-22 20:19:09 +0100

    Implement LibsService into ModalComponent

--- personal-page/src/app/cli/commandline-interface/history-displayer/skills/modal/modal.component.ts
+++ personal-page/src/app/cli/commandline-interface/history-displayer/skills/modal/modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Lib } from 'src/app/cli/services/lib.model';
+import { LibsService } from 'src/app/cli/services/libs.service';
 import { ProjectService } from 'src/app/cli/services/project.service';
 import { Project } from '../../../../services/project.model';
 
@@ -10,14 +12,18 @@ import { Project } from '../../../../services/project.model';
 export class ModalComponent {
   @ViewChild('myModal', { static: false }) modal: ElementRef;
   input: string = 'In development...';
-  relatedLibs: string[] = [];
+  relatedLibs: Lib[] = [];
   relatedProjects: Project[] = [];
 
-  constructor(private projectService: ProjectService) {}
+  constructor(
+    private projectService: ProjectService,
+    private libsService: LibsService
+  ) {}
 
   open(skill: string) {
     this.input = skill;
     this.relatedProjects = this.projectService.getRelatedProjects(skill);
+    this.relatedLibs = this.libsService.getRelatedLibs(skill);
     this.modal.nativeElement.style.display = 'block';
   }
 
