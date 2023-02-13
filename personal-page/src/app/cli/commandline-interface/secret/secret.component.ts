33d351444f68ac72cd042c1595899fe32d717251
janlingen <82512348+janlingen@users.noreply.github.com>
2023-02-13 22:33:36 +0100

    Add validation functionality to SecretComponent

--- personal-page/src/app/cli/commandline-interface/secret/secret.component.ts
+++ personal-page/src/app/cli/commandline-interface/secret/secret.component.ts
@@ -6,5 +6,20 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./secret.component.css'],
 })
 export class SecretComponent {
+  success = false;
+  wrong = false;
+  input : string = "";
+
   constructor() {}
+
+  checkInput(){
+    console.log(this.input);
+    if(this.input == "found"){
+      this.success = true;
+      this.wrong = false;
+    } else{
+      this.success = false;
+      this.wrong = true;
+    }
+  }
 }
